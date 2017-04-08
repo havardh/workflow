1009fdd07aa826d3e0948a8034e38520caff2d7f
Rolf Erik Lekang <me@rolflekang.com>
2017-04-08 16:57:41 +0200

    refactor: Move workflow setup into i3 wm

--- workflow/main.py
+++ workflow/main.py
@@ -4,27 +4,12 @@ from .window_managers import Wm
 
 
 def run(config_file, file):
-    wm = Wm()
+    window_manager = Wm()
 
-    meta = {
+    workflow = parse(config_file, meta={
         "file": file,
         "folder": find_project_folder(file),
         "test_file": get_test_file(file)
-    }
-    workspaces = parse(config_file, meta)
+    })
 
-    for workspace in workspaces:
-        wm.create_workspace(workspace)
-
-        has_all_apps = True
-        for app in workspace.apps:
-            if not wm.has_app(app):
-                has_all_apps = False
-
-        if not has_all_apps:
-            wm.clear_workspace()
-
-            wm.create_layout(workspace.layout)
-
-            for app in workspace.apps:
-                wm.open(app.cmd())
+    window_manager.setup_workflow(workflow)
