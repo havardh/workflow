9bf315f0cbcf4edbf8fe442e66c65074fbb80f76
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-04-28 09:53:55 +0200

    wip

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 // @flow
-import config from './examples/js-test';
+import examples from './examples';
 import workflow from './workflow';
 
-const [node, index, configFile, ...args] = process.argv; // eslint-disable-line no-unused-vars
-
-class InvalidArgument {
-  cause = null;
-
-  constructor(cause = 'InvalidArgument') {
-    this.cause = cause;
-  }
-}
-
-function parseArgs(config, args) { // eslint-disable-line no-shadow
-  if (typeof config === 'object' && config.length && config.length === args.length) {
-    const argsObject = {};
-    config.forEach((arg) => { argsObject[arg] = args.shift(); });
-    return argsObject;
-  } else if (typeof config === 'string' && args.length === 1) {
-    return { [config]: args[0] };
-  }
-  if (args.length && config.length) {
-    throw new InvalidArgument(`Invalid arguments. Arugments ${args.join(' ')} did not match config: ${config.join(' ')}`);
-  } else {
-    throw new InvalidArgument();
-  }
-}
-
-workflow.apply(config, parseArgs(config.args, args));
+workflow.run(examples);
--- workflow/helpers/advisor.js
+++ workflow/helpers/advisor.js
@@ -1,5 +1,7 @@
 // @flow
 /* eslint-disable import/prefer-default-export */
 export function getTestFile(file: string): string { // eslint-disable-line no-unused-vars
-  return '/home/havard/cicero/hb-advisory/frontend/advisor/test/stores/saving_product_order_form_store_tests.js';
+  return file
+    .replace('src', 'test')
+    .replace('.js', '_tests.js');
 }
