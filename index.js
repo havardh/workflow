8de0dbb5a0bf9b8fac6b7252d794531605b2342c
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-05-03 12:15:35 +0200

    refactor: add tests and refactor argument parsing

--- index.js
+++ index.js
@@ -1,88 +1,19 @@
 // @flow
 /* eslint-disable no-console */
+import I3 from './wms/i3';
+import parseArgs from './parser/args';
+import parseConfig from './parser/config';
+
 import type { SplitVConfig, SplitHConfig } from './layout';
 import type { AppConfig } from './apps';
-
-import I3 from './wms/i3';
+import type { Args } from './parser/args';
 
 export type NodeConfig = SplitVConfig | SplitHConfig | AppConfig;
-
-type Args = string | Array<string>
-
 export type WorkspaceConfig = {
   name: string,
   args: Args,
   root: NodeConfig,
 };
-
-function handleInvalidArguments(config, args) {
-  if (typeof config === 'object' && config.length && args.length !== config.length) {
-    console.log(`Invalid ARguments: Wanted ${config.join(', ')}, got ${args.join(', ')}`);
-    process.exit(1);
-  } else if (typeof config === 'string') {
-    console.log(`Invalid Arguments: Single argument '${config}' wanted, got: '${String(args)}'`);
-    process.exit(1);
-  } else {
-    console.log(`Unknown error with argument. config: ${String(config)}, args: '${String(args)}'`);
-    process.exit(1);
-  }
-}
-
-function parseArgs(
-  config: Args,
-  args: Array<string>,
-): {[string]: string} { // eslint-disable-line no-shadow
-  if (typeof config === 'object' && config.length && config.length === args.length) {
-    const argsObject = {};
-    config.forEach((arg) => { argsObject[String(arg)] = args.shift(); });
-    return argsObject;
-  } else if (typeof config === 'string' && args.length === 1) {
-    return { [config]: args[0] };
-  }
-  // $FlowTodo
-  return handleInvalidArguments(config, args);
-}
-
-
-function parseValue(value, args) { // eslint-disable-line no-shadow
-  if (typeof value === 'function') {
-    return value(args);
-  } else if (typeof value === 'number' || typeof value === 'string') {
-    return value;
-  } else if (typeof value === 'object' && value.length) {
-    return value.map(v => parseValue(v, args));
-  }
-  return value;
-}
-
-function parseApp(config, args) {
-  const { open, percent } = config;
-  if (typeof open === 'string') {
-    return { open, percent };
-  }
-  const transformedConfig = {};
-
-  Object.keys(config)
-      .filter(key => key !== 'open')
-      .forEach((key: string) => { transformedConfig[key] = parseValue(config[key], args); });
-
-  return {
-    percent,
-    class: transformedConfig.class,
-    open: open(transformedConfig),
-  };
-}
-
-function parseConfig(config: WorkspaceConfig | NodeConfig, args: {[string]:string}) {
-  if (config.root) {
-    // $FlowTodo
-    return { ...config, root: parseConfig(config.root, args) };
-  } else if (config.children) {
-    // $FlowTodo
-    return { ...config, children: config.children.map(child => parseConfig(child, args)) };
-  }
-  return parseApp(config, args);
-}
 
 function apply(config) { // eslint-disable-line no-unused-vars
   const i3 = new I3();
---
+++ test/apps/index_tests.js
@@ -0,0 +1,30 @@
+// @flow
+/* eslint-env mocha */
+import { it, describe } from 'mocha';
+import { expect } from 'chai';
+
+import { Atom, XTerm } from '../../apps';
+
+const config = { open: x => x };
+
+describe('Atom(config)', () => {
+  it('should add splitv layout to config', () => {
+    const input = { ...config };
+    const expected = { ...config, class: 'Atom' };
+
+    const actual = Atom(input);
+
+    expect(actual).to.deep.equal(expected);
+  });
+});
+
+describe('XTerm(config)', () => {
+  it('should add splith layout to config', () => {
+    const input = { ...config };
+    const expected = { ...config, class: 'XTerm' };
+
+    const actual = XTerm(input);
+
+    expect(actual).to.deep.equal(expected);
+  });
+});
