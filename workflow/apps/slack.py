dc3ecb60ed70ed7defdca990a92080bc2937535f
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-05-10 17:15:17 +0200

    chore: delete all python

--- workflow/apps/slack.py
+++
@@ -1,9 +0,0 @@
-class Slack:
-    def __init__(self):
-        self.window_class = "Slack"
-
-    def name(self):
-        return "Slack"
-
-    def cmd(self):
-        return "slack"
--- workflow/window_managers/__init__.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-system = platform.system()
-
-if system == 'Linux':
-    from .i3 import Wm  # noqa
-else:
-    raise NotImplementedError('Your window manager is not supported')
