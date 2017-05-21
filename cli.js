cacee38ae5a43bb60dc09f0d4001697eb67c5d6c
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-05-21 19:52:31 +0200

    fix: made import use default exported examples

--- cli.js
+++ cli.js
@@ -6,14 +6,14 @@ require('babel-register');
 
 try {
   // $FlowTodo
-  const examples = require('./dist/examples');
+  const examples = require('./dist/examples').default;
   // $FlowTodo
   const run = require('./dist').default;
 
   run(examples);
 } catch (error) {
   if (error.code === 'MODULE_NOT_FOUND') {
-    const examples = require('./examples');
+    const examples = require('./examples').default;
     const run = require('./index').default;
 
     run(examples);
