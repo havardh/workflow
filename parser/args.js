c69567ba69ce9104e3282465df31b4a5f2955806
Rolf Erik Lekang <me@rolflekang.com>
2017-05-21 11:00:45 +0200

    fix: Make errors behave like exceptions

--- parser/args.js
+++ parser/args.js
@@ -2,19 +2,43 @@
 /* eslint-disable no-console */
 export type Args = string | Array<string>
 
-export function InvalidArgument(
-  message: string,
-  config: Args,
-  args: Array<string>,
-) {
-  this.message = message;
-  this.config = config;
-  this.args = args;
+class BaseError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, this.constructor);
+    } else {
+      this.stack = new Error(message).stack;
+    }
+  }
 }
 
-export function InvalidConfig(message: string, config: mixed) {
-  this.message = message;
-  this.config = config;
+export class InvalidArgument extends BaseError {
+  config: Args;
+  args: Array<string>;
+
+  constructor(
+    message: string,
+    config: Args,
+    args: Array<string>,
+  ) {
+    super(message);
+    this.message = message;
+    this.config = config;
+    this.args = args;
+  }
+}
+
+export class InvalidConfig extends BaseError {
+  config: mixed;
+
+  constructor(message: string, config: mixed) {
+    super(message);
+    this.message = message;
+    this.config = config;
+  }
 }
 
 function validateArguments(config, args) {
