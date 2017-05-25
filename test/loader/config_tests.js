cb9f6ed96d69819a6b2f49f556d555a2a2d1edd0
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-05-25 12:31:15 +0200

    test: refactor to jest asserts

--- test/loader/config_tests.js
+++ test/loader/config_tests.js
@@ -1,16 +1,12 @@
 // @flow
 /* eslint-env jest */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-expressions, import/no-extraneous-dependencies */
 import os from 'os';
-import chai, { expect } from 'chai';
 import { sandbox } from 'sinon';
-import sinonChai from 'sinon-chai';
 
 import RequireWrapper from '../../util/require';
 import load from '../../loader/config';
 
-chai.use(sinonChai);
 const sinon = sandbox.create();
 
 describe('load(configFile)', () => {
@@ -41,7 +37,7 @@ describe('load(configFile)', () => {
 
     const file = load('/path/to/file.js');
 
-    expect(file).to.equal(expectedFile);
+    expect(file).toEqual(expectedFile);
   });
 
   it('should require file from ~/.workflow', () => {
@@ -52,7 +48,7 @@ describe('load(configFile)', () => {
 
     const file = load('file.js');
 
-    expect(file).to.equal(expectedFile);
+    expect(file).toEqual(expectedFile);
   });
 
   it('should require file from workflow examples', () => {
@@ -63,7 +59,7 @@ describe('load(configFile)', () => {
 
     const file = load('file.js');
 
-    expect(file).to.equal(expectedFile);
+    expect(file).toEqual(expectedFile);
   });
 
   it('should require in specific order', () => {
@@ -76,8 +72,9 @@ describe('load(configFile)', () => {
 
     // $FlowTodo
     const calls = RequireWrapper.require.getCalls();
-    expect(calls[0].args[0]).to.equal('file.js');
-    expect(calls[1].args[0]).to.equal('/home/user/.workflow/file.js');
-    expect(calls[2].args[0]).to.equal('/home/user/dev/workflow/examples/file.js');
+
+    expect(calls[0].args[0]).toEqual('file.js');
+    expect(calls[1].args[0]).toEqual('/home/user/.workflow/file.js');
+    expect(calls[2].args[0]).toEqual('/home/user/dev/workflow/examples/file.js');
   });
 });
