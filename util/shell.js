4cdc9c0b85b97db2ae4d96ea1c798f78066ff148
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-06-06 21:20:55 +0200

    feat: add feature detection for wmctrl and i3 on linux

--- util/shell.js
+++ util/shell.js
@@ -6,3 +6,7 @@ import shell from 'shelljs';
 export function exec(cmd: string) {
   return shell.exec(cmd, { silent: true, async: true });
 }
+
+export function which(cmd: string) {
+  return shell.exec(`which ${cmd}`, { silent: true, async: false });
+}
