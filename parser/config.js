83ddabbda1cb6753b220576d90b7d59a74548898
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-06-08 18:48:18 +0200

    refactor: add internal config types
    This change adds three central types to workflow. These are:
     - Config
     - LayoutNode
     - App
    
    These are the three types which are passed from the parsed user input
    into the wm layer. They describe the workflow layout and how to open
    each application at the leaf nodes.

--- parser/config.js
+++ parser/config.js
@@ -1,5 +1,6 @@
 // @flow
 import type { WorkspaceConfig, NodeConfig } from '../index';
+import type { AppConfig } from '../apps';
 
 function parseValue(value, args) { // eslint-disable-line no-shadow
   if (typeof value === 'function') {
@@ -12,17 +13,29 @@ function parseValue(value, args) { // eslint-disable-line no-shadow
   return value;
 }
 
-function parseApp(config, args) {
+export type App = {|
+  pid?: string,
+  percent: number,
+  class: string,
+  open: string,
+|};
+
+function parseApp(config: AppConfig, args): App {
   const { open, percent } = config;
-  if (typeof open === 'string') {
-    return { open, percent };
-  }
+
   const transformedConfig = {};
 
   Object.keys(config)
       .filter(key => key !== 'open')
       .forEach((key: string) => { transformedConfig[key] = parseValue(config[key], args); });
 
+  if (typeof open === 'string') {
+    return {
+      open,
+      class: transformedConfig.class,
+      percent,
+    };
+  }
   return {
     percent,
     class: transformedConfig.class,
@@ -31,16 +44,38 @@ function parseApp(config, args) {
   };
 }
 
-export default function parseConfig(
-  config: WorkspaceConfig | NodeConfig,
-  args: {[string]:string},
-) {
-  if (config.root) {
-    // $FlowTodo
-    return { ...config, root: parseConfig(config.root, args) };
-  } else if (config.children) {
-    // $FlowTodo
-    return { ...config, children: config.children.map(child => parseConfig(child, args)) };
+export type LayoutNode = {|
+  percent: number,
+  layout: string,
+  children: Array<Node> // eslint-disable-line no-use-before-define
+|};
+
+export type Node = LayoutNode | App;
+
+export type Config = {|
+  name: string,
+  root: Node
+|};
+
+function parseNode(config: NodeConfig, args: {[string]:string}): Node {
+  if (config.children) {
+    return {
+      percent: config.percent,
+      // $FlowTodo
+      layout: config.layout,
+      // $FlowTodo
+      children: config.children.map(child => parseNode(child, args)),
+    };
   }
   return parseApp(config, args);
+}
+
+export default function parseConfig(
+  config: WorkspaceConfig,
+  args: {[string]:string},
+): Config {
+  return {
+    name: config.name,
+    root: parseNode(config.root, args),
+  };
 }
