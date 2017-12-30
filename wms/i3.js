12bbef5f96377b4630a74b5e63e5b8fa69b2168f
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-12-30 09:51:38 +0100

    wip: only re-render i3 layer when it is dirty

--- wms/i3.js
+++ wms/i3.js
@@ -1,6 +1,49 @@
+/* eslint-disable no-restricted-syntax */
 import { createClient } from 'i3';
 import { file } from 'tmp-promise';
 import { outputFile } from 'fs-extra';
+import shell from 'shelljs';
+
+function matches(expected, actual) {
+  const expectedApps = {};
+  for (const app of expected) {
+    if (expectedApps[app.class]) {
+      expectedApps[app.class] += 1;
+    } else {
+      expectedApps[app.class] = 1;
+    }
+  }
+
+  const actualApps = {};
+  for (const app of actual) {
+    if (app) {
+      if (actualApps[app]) {
+        actualApps[app] += 1;
+      } else {
+        actualApps[app] = 1;
+      }
+    }
+  }
+
+  for (const app of Object.keys(expectedApps)) {
+    if (!actualApps[app] || expectedApps[app] !== actualApps[app]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function readCurrentLayout() {
+  const result = shell.exec(
+   'i3-save-tree | grep "class" | awk \'{print $3}\' | cut -c 3- | rev | cut -c 4- | rev',
+    { silent: true },
+  );
+  return result.stdout
+    .replace(/"\^/, '')
+    .replace(/\$",/, '')
+    .split('\n');
+}
 
 export default class I3 {
   constructor() {
@@ -13,12 +56,23 @@ export default class I3 {
   async apply(config) {
     this.createWorkspace(config);
 
+    // setTimeout(async () => {
+    //  if (false && this.isDirty(config.root)) {
     this.clearWorkspace();
 
     await this.createLayout(config.root);
 
     this.findApps(config.root)
-      .forEach(app => this.open(app));
+        .forEach(app => this.open(app));
+    // }
+    // }, 500);
+  }
+
+  isDirty(root) {
+    const expected = this.findApps(root);
+    const actual = readCurrentLayout();
+
+    return !matches(expected, actual);
   }
 
   createWorkspace(config) {
