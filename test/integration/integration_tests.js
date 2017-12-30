7228a35f6d3bd68039ec74be82304c0ed631a959
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-12-30 23:19:07 +0100

    test: add test script and increase test timeout

--- test/integration/integration_tests.js
+++ test/integration/integration_tests.js
@@ -10,8 +10,28 @@ expect.extend({ toMatchImageSnapshot });
 
 const platform = process.platform;
 
-test(`${platform}:term:split`, async () => {
-  await runWith('term-split', []);
+function waitFor(seconds) {
+  const waitTill = new Date(new Date().getTime() + (seconds * 1000));
 
-  expect(await take()).toMatchImageSnapshot();
+  while (waitTill > new Date());
+}
+
+describe('Integration tests', () => {
+  let originalTimeout;
+  beforeAll(() => {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  });
+
+  test(`${platform}:term:split`, async () => {
+    await runWith('term-split', []);
+
+    waitFor(2);
+
+    expect(await take()).toMatchImageSnapshot();
+  });
+
+  afterAll(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
 });
