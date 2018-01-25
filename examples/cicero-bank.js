e9fe8afda31d6ec113ab096f8deb2b6574fede92
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-01-25 22:46:43 +0100

    docs: add more examples

---
+++ examples/cicero-bank.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+import React from '../helpers/jsx';
+
+import { Workspace } from '../index';
+import { SplitV, SplitH } from '../layout';
+import { XTerm, Atom } from '../apps/linux';
+
+import { projectRoot } from '../helpers/git';
+import { getTestFile } from '../helpers/advisor';
+
+/*
+function onWorkspace() {
+  startAdvisor();
+  setCapConfig('cicero-bank.properties');
+  startCap();
+}
+*/
+
+const workspace =
+  <Workspace
+    name={'advisor:unit-test'}
+    args={'file'}
+    // componentWillMount={onWorkspace}
+  >
+    <SplitV>
+      <SplitH>
+        <Atom
+          folder={({ file }) => projectRoot(file)}
+          file={({ file }) => file}
+        />
+        <Atom
+          folder={({ file }) => projectRoot(file)}
+          file={({ file }) => getTestFile(file)}
+        />
+      </SplitH>
+      <XTerm
+        cwd={({ file }) => projectRoot(file)}
+        cmd={'npm run watch:test:base --'}
+        args={[({ file }) => getTestFile(file)]}
+      />
+    </SplitV>
+  </Workspace>;
+
+export default workspace;
