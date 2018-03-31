f8de97fcda51c95388be85432c3a516a232b158e
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-03-31 08:26:55 +0200

    test: make windows desktop comparable for integration tests

---
+++ Scripts/windows-ci-desktop-layout.ps1
@@ -0,0 +1,14 @@
+# Set prompt to '# '
+new-item -itemtype file -path $profile -force
+Set-ExecutionPolicy RemoteSigned
+echo 'function prompt {"# "}' > $PROFILE
+
+# Set PowerShell window title to 'PowerShell'
+echo '$host.ui.RawUI.WindowTitle = "PowerShell"' > $PROFILE 
+
+# Hide taskbar
+$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3'
+$v=(Get-ItemProperty -Path $p).Settings
+$v[8]=3
+&Set-ItemProperty -Path $p -Name Settings -Value $v
+&Stop-Process -f -ProcessName explorer
