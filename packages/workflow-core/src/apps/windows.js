ce84a5f430a810c0b0f83c8f121c213394f2c776
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-04-03 22:04:09 +0200

    fix: add names to windows apps

--- packages/workflow-core/src/apps/windows.js
+++ packages/workflow-core/src/apps/windows.js
@@ -1,16 +1,19 @@
 
 export const Notepad = {
   params: ["file"],
+  name: "Notepad",
   open: ({ file }) => `notepad.exe ${file || ""}`,
 };
 
 export const IExplorer = {
   params: ["url"],
+  name: "IExplorer",
   open: ({ url }) => `"C:\\Program Files\\Internet Explorer\\iexplore.exe" ${url}`,
 };
 
 export const PowerShell = {
   params: ["file"],
+  name: "PowerShell",
   open: {
     program: "powershell.exe",
     start: true,
