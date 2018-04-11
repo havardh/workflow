dbd550eaa07ee7703ffaef17025bef6c915cbb1a
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-04-11 19:25:01 +0200

    refactor: extract workflow-wm-osx module

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,40 @@
 import babel from "rollup-plugin-babel";
 import alias from "rollup-plugin-alias";
 
-export default {
-  input: "packages/workflow-core/src/index.js",
-  output: {
-    file: 'packages/workflow-core/dist/index.js',
-    format: 'cjs'
-  },
-  plugins: [
-    babel({ exclude: 'node_modules/**'}),
-    alias({ shared: __dirname + "/packages/shared"})
-  ],
+const bundles = [{
+  name: "workflow-core",
   external: [
     "lodash",
     "os",
     "fs",
+    "child_process",
+    "shelljs",
+    "python-shell",
     "babel-core",
-    "crypto"
+    "crypto",
+    "workflow-wm-i3",
+    "workflow-wm-osx"
   ]
+}, {
+  name: "workflow-wm-osx",
+  external: ["osascript", "shelljs"]
+}]
+
+function createConfig(bundle) {
+  const {name, external} = bundle;
+
+  return {
+    input: `packages/${name}/src/index.js`,
+    output: {
+      file: `packages/${name}/dist/index.js`,
+      format: 'cjs'
+    },
+    plugins: [
+      babel({ exclude: 'node_modules/**'}),
+      alias({ shared: __dirname + "/packages/shared"})
+    ],
+    external: bundle.external
+  };
 }
+
+export default bundles.map(createConfig);
