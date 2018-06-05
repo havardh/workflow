804a4ab8d4762d77a975022d39f383441d9139dc
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-06-05 23:38:44 +0200

    feat: rewrite workflow-core to make use of plugins

--- packages/workflow-core/src/apps/defaults/windows.js
+++
@@ -1,5 +0,0 @@
-import { PowerShell, IExplorer, Notepad } from '../windows';
-
-export const Terminal = PowerShell;
-export const Browser = IExplorer;
-export const TextEditor = Notepad;
---
+++ packages/workflow-core/src/transformer.js
@@ -0,0 +1,28 @@
+export async function apply(node, transformer, args, parent = undefined) {
+
+  let transformed = (await transformer.transformBefore(node, {args, parent})) || node;
+
+  let children = undefined;
+  for (const child of (node.children || [])) {
+    children = children || [];
+    children.push(await apply(child, transformer, args, node));
+  }
+
+  let root = undefined;
+  if (node.root) {
+    root = await apply(node.root, transformer, args, node);
+  }
+
+  transformed = (await transformer.transformAfter({...transformed, children, root}, {args, parent})) || { ...transformed, children, root};
+
+  return {...transformed, children, root};
+}
+
+export async function transform(transforms, config, {args}) {
+  let tree = config;
+
+  for (let transformer of transforms) {
+    tree = await apply(tree, transformer, args);
+  }
+  return tree;
+}
