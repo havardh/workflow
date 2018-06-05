57d0c47dc7b3efe522394f319f6a877b9b473dd5
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-06-05 23:41:48 +0200

    feat: rewrite wm layers to make use of workflow-layout package

--- packages/workflow-wm-windows/src/index.js
+++ packages/workflow-wm-windows/src/index.js
@@ -1,22 +1,41 @@
+/* eslint-env node */
 /* eslint-disable class-methods-use-this */
-import * as WinApi from "./win_api"
-import Tile from 'shared/tile';
 import { spawn, exec } from "child_process";
 import {difference} from "lodash";
 
-class Windows extends Tile {
+const WinApi = require("./win_api");
+import {findAllApps} from "shared/tree";
 
-  async getDesktopRect() {
+class Windows {
+
+  constructor() {
+    this.winApi = new WinApi();
+  }
+
+  async screen() {
     return new Promise((resolve) => {
-      resolve(WinApi.getDesktopRect());
+      resolve(this.winApi.getDesktopRect());
     });
   }
 
-  async setPosition({ app, position }) {
-    return new Promise((resolve) => {
-      const { x, y, width, height } = position;
+  async apply(layout) {
+    const apps = findAllApps(layout);
 
-      WinApi.setPosition(app.pid, x, y, width, height);
+    for (let app of apps) {
+      const pid = await this.runCmd(app);
+      await this.setPosition({...app, pid});
+    }
+  }
+
+  async minimizeAll() {
+    return this.winApi.minimizeAll();
+  }
+
+  async setPosition({ pid, position }) {
+    return new Promise((resolve) => {
+      const { left, top, width, height } = position;
+
+      this.winApi.setPosition(pid, left, top, width, height);
       resolve();
     });
   }
@@ -63,4 +82,4 @@ class Windows extends Tile {
   }
 }
 
-export default Windows;
+module.exports = Windows;
