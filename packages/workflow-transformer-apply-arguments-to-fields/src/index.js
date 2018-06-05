f80760dfa5a9ebccd39220246388b64d9a137aa1
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-06-05 23:35:22 +0200

    feat: implement workflow apply arguments transformer

---
+++ packages/workflow-transformer-apply-arguments-to-fields/src/index.js
@@ -0,0 +1,22 @@
+import mapValues from "lodash.mapvalues";
+
+function parseValue(value, args) {
+  if (typeof value === 'function') {
+    return value(args);
+  } else if (typeof value === 'object' && value.length) {
+    return value.map(v => parseValue(v, args));
+  }
+  return value;
+}
+
+export default class WorkflowTransformerApplyArgumentsToFields {
+  async transformBefore() {}
+  async transformAfter(node, {args}) {
+    switch (node.type) {
+      case "app": {
+        const {open, ...rest} = node;
+        return { open, ...mapValues(rest, v => parseValue(v, args)) };
+      }
+    }
+  }
+}
