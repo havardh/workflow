9ca27cc1223823c07b994dede55a8325e1cc7c2c
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-08-01 19:38:57 +0200

    fix: make osx exports of iterm2 support workflow-react package

--- packages/workflow-apps-osx/src/defaults.js
+++ packages/workflow-apps-osx/src/defaults.js
@@ -1,5 +1,5 @@
-import { iTerm2, Safari, Atom } from './apps';
+import { ITerm2, Safari, Atom } from './apps';
 
-export const Terminal = iTerm2;
+export const Terminal = ITerm2;
 export const Browser = Safari;
 export const TextEditor = Atom;
