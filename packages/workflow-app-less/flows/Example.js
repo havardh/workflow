2278122f32888d6aac2487b4454be6a7986e06de
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-08-20 18:34:47 +0200

    feat: add less ssh app

---
+++ packages/workflow-app-less/flows/Example.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+import React from 'react';
+import render, { Workspace, requireComponent } from 'workflow-react';
+
+const Terminal = requireComponent("workflow-app-terminal");
+const Less = requireComponent("workflow-app-less");
+
+export default render(
+  <Workspace name={'workflow-app-less'}>
+    <Terminal>
+      <Less follow file={__filename} />
+    </Terminal>
+  </Workspace>,
+);
