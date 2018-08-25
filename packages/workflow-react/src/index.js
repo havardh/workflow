914691b4a72eb33acbca7d8ae208636178a1a33e
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-08-25 18:51:37 +0200

    fix: remove obsolete layout module from workflow-react

--- packages/workflow-react/src/index.js
+++ packages/workflow-react/src/index.js
@@ -1,11 +1,9 @@
 import render from './render/index';
 
-import * as Layouts from './layouts/split';
-
 import { App, Layout, Workspace } from './components';
 import createComponent from './createComponent';
 import requireComponent from './requireComponent';
 
 export default render;
 
-export { App, Layout, Workspace, Layouts, createComponent, requireComponent };
+export { App, Layout, Workspace, createComponent, requireComponent };
