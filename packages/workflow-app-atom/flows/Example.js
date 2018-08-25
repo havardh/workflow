f52c048bea151dd6c9d35d651f138b19bed7a3a7
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-08-25 22:06:40 +0200

    fix: make atom example portable

--- packages/workflow-app-atom/flows/Example.js
+++ packages/workflow-app-atom/flows/Example.js
@@ -6,6 +6,6 @@ const Atom = requireComponent('workflow-app-atom');
 
 export default render(
   <Workspace name={'workflow-app-atom'}>
-    <Atom file={'~/dev/circle-game/src/gamepad.rs'} />
+    <Atom file={__filename} />
   </Workspace>
 );
