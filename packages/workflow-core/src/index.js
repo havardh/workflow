335ebed3369fd96816d07a5a93fa3de5c8bc88f7
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-08-25 19:52:43 +0200

    feat: replace loader parameter with loaders parameter in core
    Replaces the single loader parameter with the multi loaders parameter.
    A loader now is configured with an minimatch expression. The loader
    is used if the path of the flow matches the expression.

--- packages/workflow-core/src/index.js
+++ packages/workflow-core/src/index.js
@@ -3,11 +3,11 @@ import { load } from './loader';
 import { transform } from './transformer';
 import { apply, screen } from './wm';
 
-export function workflow({ resolvers, loader, argumentParser, transformers, layout, wm }) {
+export function workflow({ resolvers, loaders, argumentParser, transformers, layout, wm }) {
   return {
     resolve: async path => resolve(resolvers, path),
     alternatives: async path => alternatives(resolvers, path),
-    load: async path => load(loader, path),
+    load: async path => load(loaders, path),
     parseArguments: async (flow, argv) => argumentParser.parse(flow, argv),
     transform: async (flow, args) => transform(transformers, flow, args),
     layout: async (flow, { screen }) => layout.layout(flow, { screen }),
