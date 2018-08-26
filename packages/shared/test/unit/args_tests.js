8f027c2d93d32b797fe73cda3283149f31bd82b9
Kenny John Jacob <johnjacobkenny@gmail.com>
2018-08-26 21:14:08 +0530

    refactor: getArgName and add test cases

--- packages/shared/test/unit/args_tests.js
+++ packages/shared/test/unit/args_tests.js
@@ -5,22 +5,14 @@ describe('args(args)', () => {
   it('should parse single character arguments correctly', () => {
     const input = ['hello', 'world', '-c', 'config', '-f', 'filename', 'otherargs'];
     const expectedOutput = {
-      named: { c: 'config', f:'filename' },
+      named: { c: 'config', f: 'filename' },
       positional: ['hello', 'world', 'otherargs'],
     };
     expect(args(input)).toEqual(expectedOutput);
   });
 
   it('should parse word arguments correctly', () => {
-    const input = [
-      'hello',
-      'world',
-      '--config',
-      'config',
-      '--filename',
-      'filename',
-      'otherargs',
-    ];
+    const input = ['hello', 'world', '--config', 'config', '--filename', 'filename', 'otherargs'];
     const expectedOutput = {
       named: { config: 'config', filename: 'filename' },
       positional: ['hello', 'world', 'otherargs'],
@@ -39,8 +31,20 @@ describe('getArgName(arg)', () => {
     expect(getArgName('-c')).toBe('c');
   });
 
-  it('should extract the argument name for word arg', () => {
+  it('should extract the argument name for single word arg', () => {
     expect(getArgName('--config')).toBe('config');
+  });
+
+  it('should extract the argument name for multi word arg', () => {
+    expect(getArgName('--some-long-name')).toBe('some-long-name');
+  });
+
+  it('should throw an error if - is succeded by a word', () => {
+    expect(() => getArgName('-config')).toThrowError();
+  });
+
+  it('should throw an error if -- is succeded by a single character', () => {
+    expect(() => getArgName('--c')).toThrowError();
   });
 });
 
