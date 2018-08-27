9043dafe793cbf2ac05438415194d9bd54df3dbf
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-08-27 15:06:39 +0200

    docs: update ReactExample.js

--- packages/create-workflow-home/files/static/flows/ReactExample.js
+++ packages/create-workflow-home/files/static/flows/ReactExample.js
@@ -3,24 +3,17 @@
 import React from 'react';
 import render, { Workspace, requireComponent } from 'workflow-react';
 
-const { SplitH } = requireComponent('workflow-layout-tiled');
-const { TextEditor, Browser } = requireComponent('workflow-apps-defaults');
-
-/*
-  NB this workflow requires the library workflow-react
-  Run npm i workflow-react from your workflow homefolder
-*/
-
-// Docs: https://github.com/havardh/workflow/blob/master/packages/workflow-react/Readme.md
+const { SplitH, SplitV } = requireComponent('workflow-layout-tiled');
+const { TextEditor, Browser, Terminal } = requireComponent('workflow-apps-defaults');
 
 export default render(
   <Workspace name={'workflow-react-example'}>
-    <SplitH percent={1}>
-      <TextEditor percent={0.2} file={__filename} />
-      <Browser
-        percent={0.8}
-        url={'https://github.com/havardh/workflow/tree/master/packages/workflow-cmd'}
-      />
-    </SplitH>
+    <SplitV percent={1.0}>
+      <SplitH percent={0.8}>
+        <TextEditor percent={0.5} file={__filename} />
+        <Browser percent={0.5} url={'https://github.com/havardh/workflow'} />
+      </SplitH>
+      <Terminal percent={0.2} cwd={__dirname} />
+    </SplitV>
   </Workspace>
 );
