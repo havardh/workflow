aa2ab4cc5f3bfc04bf311e04790398cd98601aa4
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-03 20:00:38 +0200

    feat(workflow-app-spotify): add support for i3

--- packages/workflow-app-spotify/flows/Example.js
+++ packages/workflow-app-spotify/flows/Example.js
@@ -2,10 +2,13 @@
 import React from 'react';
 import render, { Workspace, requireComponent } from 'workflow-react';
 
-const Emacs = requireComponent('workflow-app-spotify');
+const Spotify = requireComponent('workflow-app-spotify');
+const { Playlist } = Spotify;
 
 export default render(
-  <Workspace name={'workflow-app-example'}>
-    <Emacs file={__filename} />
+  <Workspace name={'workflow-wat'}>
+    <Spotify minimized>
+      <Playlist user={'spotify'} id={'37i9dQZF1DX1j96SzHv3js'} play />
+    </Spotify>
   </Workspace>
 );
--- packages/workflow-app-spotify/src/index.js
+++ packages/workflow-app-spotify/src/index.js
@@ -1,12 +1,31 @@
 /* eslint-disable no-unused-vars */
-const emacs = {
+
+const Playlist = {
   type: 'app',
-  name: 'Emacs',
-  xClass: 'Emacs',
-  params: ['file'],
-  open: ({ file }, context, children) => {
-    return `emacs -nw ${file}`;
+  name: 'Playlist',
+  params: ['user', 'id'],
+  open: ({ user, id }, context, children) => {
+    return `spotify:user:${user}:playlist:${id}`;
   },
 };
 
-export default emacs;
+const spotify = {
+  type: 'app',
+  name: 'Spotify',
+  xClass: 'Spotify',
+  params: ['minimized'],
+  open: ({ minimized }, context, children) => {
+    if (children.length !== 1) {
+      throw new Error('Spotify does not support more or less than one child node');
+    }
+
+    const [child] = children;
+    const uri = child.open(child, context, child.children);
+
+    return `spotify --uri='${uri}' &`;
+  },
+};
+
+export default spotify;
+
+export { Playlist };
