7b074da3f5a3624b65c6d08e45024a537ddc27bd
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-04 12:06:40 +0200

    feat(workflow-app-spotify): add osx support

--- packages/workflow-app-spotify/src/index.js
+++ packages/workflow-app-spotify/src/index.js
@@ -30,6 +30,7 @@ const spotify = {
   params: ['minimized'],
   open: platform({
     'linux-i3': linux,
+    'osx-default': osx,
   }),
 };
 
@@ -42,6 +43,44 @@ function linux({ mimimized }, context, children) {
   }
 }
 
+async function osx(app, context, children) {
+  const uri = getUri(children, context);
+
+  await context.run(
+    (app, uri) => {
+      const se = Application('System Events');
+      const window = se.processes['Spotify'].windows[0];
+
+      const spotify = Application('Spotify');
+      spotify.activate();
+
+      const { x, y, width, height } = app.position;
+      window.position = [x, y];
+      window.size = [width, height];
+
+      if (uri) {
+        spotify.playTrack(uri);
+      }
+
+      if (!app.play) {
+        spotify.pause();
+      } else {
+        spotify.play();
+      }
+
+      if (app.minimized) {
+        delay(0.1);
+        se.keystroke('m', { using: 'command down' });
+      }
+      if (app.fullscreen) {
+        delay(0.1);
+        se.keystroke('f', { using: ['control down', 'command down'] });
+      }
+    },
+    app,
+    uri
+  );
+}
 
 export default spotify;
 
