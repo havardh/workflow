8a27750ea841160f84e85e87f67dc5851e9cb5e1
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-10 12:38:07 +0200

    feat(workflow-cmd): add verbose output of version command
    The verbose mode for the version command will output the versions of the
    depenencies in the `package.json` of the workflow-home directory.

--- packages/workflow-cmd/src/commands/version.js
+++ packages/workflow-cmd/src/commands/version.js
@@ -1,8 +1,24 @@
 /* eslint-env node */
-import { baseFolder } from 'shared/env';
+import { baseFolder, configPath } from 'shared/env';
 import readPkgUp from 'read-pkg-up';
 
-export default async function version() {
+export default async function version({ verbose }) {
   const { pkg } = await readPkgUp({ cwd: __dirname });
-  console.log(pkg.version);
+  console.log('workflow:', pkg.version);
+
+  if (verbose) {
+    console.log();
+    console.log('Config:', configPath);
+    console.log();
+    console.log(`Dependencies: `);
+    await printDependenciesVersion();
+  }
+}
+
+async function printDependenciesVersion() {
+  const { pkg } = await readPkgUp({ cwd: baseFolder });
+
+  for (let [key, value] of Object.entries(pkg.dependencies)) {
+    console.log(` - ${key}: ${value}`);
+  }
 }
