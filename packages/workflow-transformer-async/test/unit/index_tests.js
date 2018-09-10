61f9f4ceae8d81d61e8889b42aa882a31300dae1
Rolf Erik Lekang <me@rolflekang.com>
2018-09-10 16:00:36 +0200

    test: Add tests for async transformer

---
+++ packages/workflow-transformer-async/test/unit/index_tests.js
@@ -0,0 +1,31 @@
+import WorkflowTransformerAsync from '../../src';
+
+let transformer;
+beforeAll(() => {
+  transformer = new WorkflowTransformerAsync();
+});
+
+test('WorkflowTransformerAsync should transform to the value that loader resolves when type is "async"', async () => {
+  const open = () => {};
+
+  const transformed = await transformer.transformBefore({
+    type: 'async',
+    loader: async () => ({ type: 'app', name: 'Iterm', open }),
+    percent: 0.5,
+  });
+
+  expect(transformed).toEqual({
+    type: 'app',
+    name: 'Iterm',
+    open,
+    percent: 0.5,
+  });
+});
+
+test('WorkflowTransformerAsync should do nothing when type is not "async"', async () => {
+  const app = { type: 'app', name: 'Iterm', open: () => {} };
+
+  const transformed = await transformer.transformBefore(app);
+
+  expect(transformed).toEqual(app);
+});
