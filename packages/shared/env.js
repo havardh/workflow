f2bf31c42f2cb30d6a3bf9156ce7a2c5f9c47e3d
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-10 08:45:32 +0200

    feat: read config file from '--config, -c' option
    Read the configuration file from the path specified at `--config/-c` if given.
    Fixes #82

--- packages/shared/env.js
+++ packages/shared/env.js
@@ -1,7 +1,9 @@
 /* eslint-env node */
 import { execSync } from 'child_process';
+import args from './args';
 import { which } from './shell';
 import os from 'os';
+import { dirname, join } from 'path';
 
 export const platform = process.platform;
 
@@ -39,6 +41,29 @@ export const dev = process.env.WORKFLOW_DEV_MODE === 'true';
 export const homedir = process.env.WORKFLOW_HOME;
 export const devhomedir = process.env.WORKFLOW_DEV_HOME;
 
-export const baseFolder = dev
-  ? devhomedir || `${__dirname}/../../workflow-template`
-  : homedir || `${os.homedir()}/.workflow`;
+const { config } = args(process.argv).named;
+
+export let configPath;
+export let baseFolder;
+(() => {
+  if (dev) {
+    if (devhomedir) {
+      baseFolder = devhomedir;
+      configPath = join(devhomedir, 'config.js');
+    } else {
+      baseFolder = join(__dirname, '..', '..', 'workflow-template', 'config.js');
+      configPath = join(baseFolder, 'config.js');
+    }
+  } else {
+    if (config) {
+      baseFolder = dirname(config);
+      configPath = config;
+    } else if (homedir) {
+      baseFolder = homedir;
+      configPath = join(baseFolder, 'config.js');
+    } else {
+      configPath = join(os.homedir(), '.workflow', 'config.js');
+      baseFolder = `${os.homedir()}/.workflow`;
+    }
+  }
+})();
