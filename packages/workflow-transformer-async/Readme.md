01a298f721bf1d3b01ed22df9c756acfa78167d7
Rolf Erik Lekang <me@rolflekang.com>
2018-09-10 14:06:10 +0200

    feat: Add workflow-transformer-async
    This enables type: "async" with a property loader that is an async
    function used to load a workflow plugin like app or layout.
    
    Relates to #132

---
+++ packages/workflow-transformer-async/Readme.md
@@ -0,0 +1,18 @@
+# workflow-transformer-async
+
+## Installation
+
+```
+npm install workflow-transformer-async
+```
+
+Add the plugin to your `~/.workflow/config.js` file.
+
+```
+import WorkflowTranformerAsync from "workflow-transformer-async";
+
+export default {
+  transformers: [new WorkflowTranformerAsync()]
+  ...
+};
+```
---
+++ packages/workflow-transformer-async/flows/Example.js
@@ -0,0 +1,34 @@
+/* eslint-env node */
+import { Terminal } from 'workflow-apps-defaults';
+
+export default {
+  name: 'workflow-example',
+  type: 'workspace',
+  children: [
+    {
+      type: 'layout',
+      layout: 'splith',
+      percent: 1.0,
+      children: [
+        {
+          type: 'async',
+          loader: async () => ({
+            ...Terminal,
+            cwd: __dirname,
+            cmd: 'pwd',
+            percent: 0.5,
+          }),
+        },
+        {
+          type: 'async',
+          loader: async () => ({
+            ...Terminal,
+            cwd: __dirname,
+            cmd: 'pwd',
+            percent: 0.5,
+          }),
+        },
+      ],
+    },
+  ],
+};
