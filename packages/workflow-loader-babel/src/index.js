46030b097eada8d21823b3d86336a60f8f92cb7c
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-11 15:34:49 +0200

    feat(workflow-loader-babel): support loading from {source} argument
    This allows us to load flow from a resolver that resolves source of the flow
    directly instead of a path to load from. This enables us to ship the `clipboard`
    resolver which returns the source code and not the path to any file.

--- packages/workflow-loader-babel/src/index.js
+++ packages/workflow-loader-babel/src/index.js
@@ -1,17 +1,30 @@
 /* eslint-env node */
 /* eslint-disable import/no-dynamic-require */
 
+import { transform } from 'babel-core';
+import requireFromString from 'require-from-string';
+
 export default class WorkflowLoaderBabel {
   constructor({ config }) {
     this.config = config;
     this.registered = false;
   }
 
-  async load(path) {
+  async load(arg) {
     if (!this.registered) {
       require('babel-register')(this.config);
       this.registered = true;
     }
-    return require(path);
+
+    if (typeof arg === 'string') {
+      return require(arg);
+    } else if (typeof arg.path === 'string') {
+      return require(arg.path);
+    } else if (typeof arg.source === 'string') {
+      const { code } = transform(arg.source, this.config);
+      return requireFromString(code);
+    } else {
+      throw new Error('Could not load from arg', JSON.stringify(arg));
+    }
   }
 }
