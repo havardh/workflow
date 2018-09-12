73d74a5f222b49a06455c86cf565dd2d40b51488
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-12 21:59:29 +0200

    fix(workflow-react): hide craeteComponentRecusive function
    We do not want to expose this from the workflow-react package at this moment.

--- packages/workflow-react/src/index.js
+++ packages/workflow-react/src/index.js
@@ -1,17 +1,9 @@
 import render from './render/index';
 
 import { App, Layout, Workspace, Async } from './components';
-import { createComponent, createComponentRecursive } from './createComponent';
+import { createComponent } from './createComponent';
 import requireComponent from './requireComponent';
 
 export default render;
 
-export {
-  App,
-  Layout,
-  Workspace,
-  Async,
-  createComponent,
-  createComponentRecursive,
-  requireComponent,
-};
+export { App, Layout, Workspace, Async, createComponent, requireComponent };
