2ba1d3b0a26d757b2cff3a0f509bcafed96d0068
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-16 21:26:38 +0200

    fix(workflow-app-terminal): support async open

--- packages/workflow-app-terminal/src/index.js
+++ packages/workflow-app-terminal/src/index.js
@@ -6,7 +6,7 @@ async function evaluate(children, context) {
   const { platform } = context;
   const wm = 'terminal';
 
-  return child.open(child, { platform, wm }, child.children);
+  return await child.open(child, { platform, wm }, child.children);
 }
 
 const Terminal = {
@@ -16,7 +16,7 @@ const Terminal = {
   params: ['cwd', 'cmd'],
   open: async ({ cwd, cmd, args, ...rest }, context, children) => {
     if (context.wm === 'terminal') {
-      return `${cwd}; clear; ${cmd || evaluate(children, context)}`;
+      return `cd ${cwd || ''}; clear; ${cmd || (await evaluate(children, context))}`;
     } else {
       return DefaultTerminal.open({ cwd, cmd, args, ...rest }, context, children);
     }
