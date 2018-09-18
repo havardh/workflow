b050804d11ac1c0f4c52aff47418a7400384c6f9
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-18 14:46:44 +0200

    fix: rebase to be compatible with 2.x

--- packages/workflow-app-xv/flows/Example.js
+++ packages/workflow-app-xv/flows/Example.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import render, { Workspace, requireComponent } from 'workflow-react';
+import { render, Workspace, requireComponent } from 'workflow-react';
 import { readdir } from 'fs';
 import { promisify } from 'util';
 import { join } from 'path';
@@ -7,11 +7,11 @@ import { join } from 'path';
 const readdirAsync = promisify(readdir);
 
 const { Flex } = requireComponent('workflow-layout-yoga');
-const ImageViewer = requireComponent('workflow-app-xv');
+const { XV } = requireComponent('workflow-app-xv');
 
 const folder = '/home/havard/docs/pictures';
 
-export default readdirAsync(folder)
+export const flow = readdirAsync(folder)
   .then(files => files.filter(file => file.endsWith('.jpg')))
   .then(files =>
     render(
@@ -25,7 +25,7 @@ export default readdirAsync(folder)
           }}
         >
           {files.map(file => (
-            <ImageViewer
+            <XV
               key={file}
               style={{ height: '300px', width: '450px', margin: '30px' }}
               file={join(folder, file)}
