95a4266728ff6718a2608d660aafa2711de9441d
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-18 12:42:12 +0200

    test: update config file format for `workflow-cmd` tests

--- packages/workflow-cmd/test/unit/__fixtures__/config.js
+++ packages/workflow-cmd/test/unit/__fixtures__/config.js
@@ -2,18 +2,20 @@
 /* eslint-disable no-console */
 
 module.exports = {
-  resolvers: [{ resolve: flow => flow }],
-  loaders: [
-    {
-      loader: {
-        load: flow => ({
-          flow: JSON.parse(flow),
-        }),
+  config: {
+    resolvers: [{ resolve: flow => flow }],
+    loaders: [
+      {
+        loader: {
+          load: flow => ({
+            flow: JSON.parse(flow),
+          }),
+        },
       },
-    },
-  ],
-  argumentParser: { parse: () => {} },
-  transformers: [],
-  layout: { layout: layout => layout },
-  wm: { screen: () => {}, apply: flow => console.log(JSON.stringify(flow, null, 2)) },
+    ],
+    argumentParser: { parse: () => {} },
+    transformers: [],
+    layout: { layout: layout => layout },
+    wm: { screen: () => {}, apply: flow => console.log(JSON.stringify(flow, null, 2)) },
+  },
 };
