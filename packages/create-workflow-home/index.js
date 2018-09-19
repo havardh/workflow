463471d4afeb64774152e8b5d498ace863f29ccc
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-19 09:49:21 +0200

    chore: make create-workflow-home work without building sources

--- packages/create-workflow-home/index.js
+++ packages/create-workflow-home/index.js
@@ -6,6 +6,7 @@ try {
   module.exports = require('./dist');
 } catch (error) {
   if (error.code === 'MODULE_NOT_FOUND') {
+    require('babel-register');
     module.exports = require('./src/index');
   } else {
     throw error;
